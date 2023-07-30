85c5ab5d686816e2e858f12ac704b8030f98b25b
tzptzptzptzptzp <tzp2539@gmail.com>
2023-07-30 23:22:54 +0900

    ✨ ルーティングを定義

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+// ポート番号の定義
+const PORT = 3000;
+
 router.get("/", (req, res) => {
   console.log(
-    `Access to http://localhost:${PORT}${req.url} has been detected.`
+    `Access to http://localhost:${PORT}${req.baseUrl} has been detected.`
   );
   res.send(
-    `<h1>The browser is accessing <a href="http://localhost:${PORT}${req.url}">http://localhost:${PORT}${req.url}</a>.</h1>`
+    `<h1>The browser is accessing <a href="http://localhost:${PORT}${req.baseUrl}">http://localhost:${PORT}${req.baseUrl}</a>.</h1>`
   );
 });
 
 router.get("/info", (req, res) => {
   console.log(
-    `Access to http://localhost:${PORT}${req.url} has been detected.`
+    `Access to http://localhost:${PORT}${req.baseUrl}${req.url} has been detected.`
   );
   res.send(
-    `<h1>The browser is accessing <a href="http://localhost:${PORT}${req.url}">http://localhost:${PORT}${req.url}</a>.</h1>`
+    `<h1>The browser is accessing <a href="http://localhost:${PORT}${req.baseUrl}${req.url}">http://localhost:${PORT}${req.baseUrl}${req.url}</a>.</h1>`
   );
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/user");
 
 // ポート番号の定義
 const PORT = 3000;
 
+// ルーティングの定義
 app.get("/", (req, res) => {
   console.log(
-    `Access to http://localhost:${PORT}${req.url} has been detected.`
+    `Access to http://localhost:${PORT}${req.baseUrl} has been detected.`
   );
   res.send(
-    `<h1>The browser is accessing <a href="http://localhost:${PORT}${req.url}">http://localhost:${PORT}${req.url}</a>.</h1>`
+    `<h1>The browser is accessing <a href="http://localhost:${PORT}${req.baseUrl}">http://localhost:${PORT}${req.baseUrl}</a>.</h1>`
   );
 });
+
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server Running http://localhost:${PORT}`);
